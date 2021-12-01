1a37091737a78e98a60ab00b94b87232ff52d821
Finn <finnmorin@uvic.ca>
2021-12-01 00:27:41 +0000

    Server parses correct request data

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, Response
 from ServerUser import User, UserStore
 
 app = Flask(__name__)
@@ -13,8 +13,19 @@ def hello_world():
 @app.route("/signup", methods=["POST"])
 def signup():
     try:
-        store.signup(request.data)
-        return "Success"
+        data = {
+            "username": request.form.get("username"),
+            "identity": request.form.get("identity"),
+            "pk_sig":   request.form.get("pk_sig"),
+            "signed_pk":    request.form.get("signed_pk"),
+            "prekeys":  request.form.getlist("prekeys")
+        }
+        for k,v in data.items():
+            if v == None:
+                return Response(f"Missing key {k}")
+        
+        store.signup(data)
+        return Response("Success")
     except Exception as e:
         print(e)
     
